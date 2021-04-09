e6e33b404a1f9a6dfbc8aa003c60f7214065bb3f
connor59 <77357815+connor59@users.noreply.github.com>
2021-04-09 01:48:21 -0400

    I have created the Main Wagon Vs Wild Code
    I have been experimenting on my own with code and this is what I have come up with so far.
    You can see a basic items list with months, food and items within the game.
    I plan on creating functions that tell the player how many miles have been traveled so far and how much food has been used along the journey.
    
    Hopefully I can implement all this and im not biting off more than I can chew.

---
+++ WagonvsWild.py
@@ -0,0 +1,56 @@
+# I begin coding here. 
+
+import random 
+
+
+miles_to_travel = 1457
+miles_traveled = 0
+min_miles = 5
+max_miles = 25
+
+food_remaining = 1000
+
+min_food_used = 25
+max_food_used = 50
+scavenge_food = 70
+hunting_food = 100
+food_per_day = 15
+
+Diseases_encountered = 0
+current_diseases = 0
+total_diseases = 10
+
+
+Months_list = ["test", "January", "Febuary", "March", "April", "May", 
+"June", "July", "August", "September", "October", "November", "December"]
+
+
+month_type1 = [1, 3, 5, 7, 8, 10, 12]
+month_type2 = [4, 6, 9, 11]
+month_type3 = [2]
+
+# Items within Game
+
+spare_wheel = 0 
+shovel = 0 
+kentucky_rifle = 0 
+pick_axe = 0 
+tent = 0 
+fire_wood = 0
+shoes = 0
+spear = 0 
+bow = 0 
+lazer_rifle = 0
+magic = 0
+
+
+
+
+
+
+
+print("--------------------------------- Wagon Vs Wild ----------------------------------------")
+
+welcome1 = ("The year is 1837, and a new state has been recently admitted to the union.\n Reading the new and exiting prospects of a life out west, you begin to make preperations to leave the state of Rhode Island.")
+welcome2 = ("You are about to begin the journey from Warwick Rhode Island to Hot Springs Arkansas.\n The journey will be perilous, but settling out west offers better prospects than Rhode Island could ever offer at this point in your lives.")
+
