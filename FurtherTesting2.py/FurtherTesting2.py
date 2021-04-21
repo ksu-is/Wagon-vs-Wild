d90f20ba1c93f04ace207f6cf47ebbb7f13febef
connor59 <77357815+connor59@users.noreply.github.com>
2021-04-21 01:06:31 -0400

    Added River Crossing Event
    This event hinges on the player having a spare wheel within the inventory, having this item will allow the player to continue.
    
    Notice random_event, this is sort of a roll based encounter, where the player will need to get lucky to pass, you can technically pass this stage without a spare wheel, however if your roll is unlucky then the game ends.

--- FurtherTesting2.py/FurtherTesting2.py
+++ FurtherTesting2.py/FurtherTesting2.py
@@ -1,41 +1,19 @@
 import time 
 import random
 
-def wizard_encounter(supply):
-    color_ans = ["red", "orange", "yellow", "green", "violet", "indigo", "blue"]
-    print("In the distance you see a few robed men in pointy hats, they sit atop a wagon riding towards you.") 
-    time.sleep(3)
-    print("One wears leather hides, the next wears long silk robes, the last is dressed similar to other travilers you have seen along the road.")
-    time.sleep(7)
-    print("The normally dressed Wizard introduces himslef as: Tim the Terrible!")  
-    print("He says, 'In order to pass you must answer me these questions three, lest we strike you with magic!'")
-    time.sleep(7)
-    print("What, is your favorite Color?")
-    print("What, is the air speed velocity of an unlaiden swallow?")
-    print("What, is the capital of Syria?")
-    time.sleep(8)
-    print("At this point you are fairly bemused, until one of them strikes a tree with lightning nearby.")
-    user = input("Your response to the first question.").lower()
-    if user in color_ans:
-        
-        print("Correct on the first question.")
-    else:
-        print("Wrong, says the wizard in long silk robes, he strikes you with lightning.")
-    user1 = input("Enter a numeric Value").lower()
-    if user1 == "24":
-        
-        print(str(user1) + " miles per hour is correct, now answer the final answer.")
-    else:
-        print("Wrong, says the wizard in long silk robes, he strikes you with lightning.")
-    user2 = input("Your response to the third question.").lower()
-    if user2 == "damascus":
-        
-        print("Correct, you have answered all our questions and may go in peace!")
-        time.sleep(4)
-        print("You quickly gather the reins of the horse atop the wagon and continue along the path")
+def river_crossing():
+    global spare_wheel
+    random_event = ["crossed", "crossed", "crossed", "crossed", "crossed", "fail", "fail"]
+    event = random.choice(random_event)
+    if event.startswith("c"):
+        print("You have Forded the river, although rocky and dangerous, the wagon made it across.")
         time.sleep(3)
-    else:
-        print("Wrong, says the wizard in long silk robes, he strikes you with lightning.")
+    elif event.startswith("f"):
+        print("You have run into trouble on the river, a wagon wheel breaks.")
+        spare_wheel -= 1
+        if spare_wheel == 0:
+            print("You do not have a spare wagon wheel, the wagon begins floating along the river.")
+            time.sleep(3)
+            print("Eventually, your wagon hits a rock and breaks apart. Your journey has come to an end.")
 
-
-wizard_encounter()
+river_crossing()
--- WagonvsWild.py
+++ WagonvsWild.py
@@ -166,6 +166,23 @@ def combat_z(supply):
                 continue_journey(supply)
 
 
+def river_crossing(supply):
+    global spare_wheel
+    random_event = ["crossed", "crossed", "crossed", "crossed", "crossed", "fail", "fail"]
+    event = random.choice(random_event)
+    if event.startswith("c"):
+        print("You have Forded the river, although rocky and dangerous, the wagon made it across.")
+        time.sleep(3)
+        continue_journey(supply)
+    elif event.startswith("f"):
+        print("You have run into trouble on the river, a wagon wheel breaks.")
+        spare_wheel -= 1
+        if spare_wheel == 0:
+            print("You do not have a spare wagon wheel, the wagon begins floating along the river.")
+            time.sleep(3)
+            print("Eventually, your wagon hits a rock and breaks apart. Your journey has come to an end.")
+            ending_choice()
+
 def wizard_encounter(supply):
     color_ans = ["red", "orange", "yellow", "green", "violet", "indigo", "blue"]
     print("In the distance you see a few robed men in pointy hats, they sit atop a wagon riding towards you.") 
